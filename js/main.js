043b35ff96261178280b1391ab557df5e09f9ff9
Ami <ami.nakano@outlook.com>
2019-08-16 21:01:19 +0200

    foreign pages created

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const pageChange = () =>{
+  let path = document.lang.lang_option.value;
+  location.href = path;
+}
+function dropsort() {
+    var browser = document.sort_form.sort.value;
+    location.href = browser
+}
