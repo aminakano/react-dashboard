948fc86430938a7a42a1192270993037d17a6ade
Ami Nakano <ami.nakano@outlook.com>
2021-01-26 08:26:50 +0100

    reordered cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -41,22 +41,22 @@ const Cards = ({ data }) => {
 
   let { filteredArr, counter } = filterArr(holdingTokens, myHoldings);
   
-  console.log(filteredArr);
+  console.log(filteredArr, counter);
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={4} className={styles.card}>
+        <Grid item component={Card} xs={12} md={3} className={styles.card}>
+            <Typography variant="h5" className={styles.title}>Portfolio</Typography>
+            <Chart arr={filteredArr} />
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} lg={4} xl={3} className={styles.card}>
           <Typography variant="h5" className={styles.title}>Your Assets</Typography>
           <Typography variant="h4" align="right" className={styles.title}>
             ${counter[0]}.<span className={styles.decimal}>{counter[1]}</span>
           </Typography>
           <AssetTable arr={filteredArr} />
         </Grid>
-        <Grid item xs={12} md={3} component={Card} className={styles.card}>
-          <Typography variant="h5" className={styles.title}>Today's Crypto Prices</Typography>
-          <TableCard arr={arr}/>
-        </Grid>
-        <Grid item component={Card} xs={12} md={2} className={styles.card}>
+        <Grid item component={Card} xs={12} md={3} lg={2} xl={3} className={styles.card}>
           <CardContent>
             <MyButton
               tip="Edit coin"
@@ -78,6 +78,15 @@ const Cards = ({ data }) => {
             <Coin coinName={coin} />
           </CardContent>
         </Grid>
+         <Grid item component={Card} xs={12} md={3} className={styles.card}>
+          <CardContent>
+            <EmbedTwitter id="NexoFinance" name="Nexo"/>
+          </CardContent>
+        </Grid>
+        <Grid item xs={12} md={3} component={Card} className={styles.card}>
+          <Typography variant="h5" className={styles.title}>Today's Crypto Prices</Typography>
+          <TableCard arr={arr}/>
+        </Grid>
         <Grid item component={Card} xs={12} md={3} className={styles.card}>
           <CardContent>
             <Typography variant="h5" className={styles.title}>This is my card</Typography>
@@ -85,23 +94,14 @@ const Cards = ({ data }) => {
             <img src={img} alt="Diamond" />
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={styles.card}>
-          <CardContent>
-            <EmbedTwitter id="NexoFinance" name="Nexo"/>
-          </CardContent>
-        </Grid>
+       
         <Grid item component={Card} xs={12} md={3} className={styles.card}>
           <Typography variant="h5" className={styles.title}>Portfolio</Typography>
           {/* <CardContent> */}
             <Chart arr={filteredArr} />
           {/* </CardContent> */}
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={styles.card}>
-          <CardContent>
-            <Typography variant="h5">This is my card</Typography>
-            <Typography variant="body2">Hey!</Typography>
-          </CardContent>
-        </Grid>
+        
         <Grid item component={Card} xs={12} md={3} className={styles.card}>
           <CardContent>
             <Typography variant="h5">This is my card</Typography>
