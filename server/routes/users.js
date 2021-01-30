3419d2ebd98e4d811e4b8a6d56cd5ae232f8f438
Ami Nakano <ami.nakano@outlook.com>
2021-01-30 16:16:37 +0100

    server setup started

---
+++ server/routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Exercise = require("../models/exercise.model");
+let User = require("../models/user.model");
+
+// first endpoint for incoming http://localhost:5000/users get request
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// for http://localhost:5000/users/add post request
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+
+  const newUser = new User({ username });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Environment variables in .env file
+require("dotenv").config();
+
+// Express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // allows us to parse JSON
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+// once the server is conneced to the database it logs
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exerciseRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exerciseRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
