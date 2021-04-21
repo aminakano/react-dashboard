fe361c9212beddb46adda3a6d77f838c59ae3c38
Ami Nakano <ami.nakano@outlook.com>
2021-04-21 22:49:39 +0200

    working on form

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { TextField, Button, CircularProgress, Grid, Typography, Paper } from "@material-ui/core";
 
 const Settings = () => {
   return (
     <div>
-      Settings
+      <Grid container>
+        <Grid item>
+          <Typography variant="h2">Settings</Typography>
+          <h3>Crypto Holdlings</h3>
+          <TextField
+            id="holdings"
+            name="holdings"
+            type="text"
+            label="Crypto Holdings"
+            // helperText={errors.email}
+            // error={errors.email ? true : false}
+            // className={styles.TextField}
+            // value={this.state.email}
+            // onChange={this.handleChange}
+            fullWidth
+          />
+          <h3>Price chart to Follow</h3>
+          <h3>Twitter to Follow</h3>
+        </Grid>
+      </Grid>
     </div>
   )
 }
